afba7df43cd0476f8b36396e26024c24a992940e
marwank270 <marwan.kaouachi@gmail.com>
2026-05-16 16:22:41 +0200

    ajout de la fonction `startTimer()`

--- js/Game.js
+++ js/Game.js
@@ -5,8 +5,12 @@ import {ApiService} from './ApiService.js';
 export class Game {
   /**
    * @type {number} id identifiant de la partie en cours
+   * @type {number} timerId identifiant du timer de la partie en cours (pour pouvoir l'arrêter)
+   * @type {number} elapsedTime temps écoulé en centièmes de seconde
    */
   #id;
+  #timerId;
+  #elapsedTime = 0;
 
   async endGame() {
     // Todo À compléter
@@ -32,13 +36,32 @@ export class Game {
    */
   startGame(id, collectionName) {
     this.#id = id;
+    this.#elapsedTime = 0;
+    this.startTimer();
 
-    // @todo lancer le chronomètre
     // @todo afficher les cartes (face cachées) sur le plateau de jeu
     // @todo ajouter un bouton d'abandon
 
   }
 
   // Todo À compléter
+
+  startTimer() {
+    const timer = document.querySelector('.game-timer');
+
+    // @note utiliser hors prod car unsafe d'après les chad de chez mozzila
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/setInterval
+    this.#timerId = setInterval(() => {
+      this.#elapsedTime++;
+      const min = Math.floor(this.#elapsedTime / 600);
+      const sec = Math.floor((this.#elapsedTime % 600) / 10);
+      const dix = this.#elapsedTime % 10;
+      const minStr = min < 10 ? `0${min}` : `${min}`;
+      const secStr = sec < 10 ? `0${sec}` : `${sec}`;
+      // Update le timer sur la page
+      timer.textContent = `${minStr}:${secStr}.${dix}`;
+      // Mettre à jour le timer toutes les 100ms
+    }, 100);
+  }
 
 }
